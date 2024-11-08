0b60750470c6ca07b160792e59e1a8ee380cbc20
SabitCanTurunc <turuncsc@gmail.com>
2024-11-08 07:28:14 +0300

    domain page{ greeting message, product adding, sheet ,table,tabs}

--- src/app/(dashboard)/settings/[domain]/page.tsx
+++ src/app/(dashboard)/settings/[domain]/page.tsx
@@ -1,6 +1,8 @@
 import { onGetCurrentDomainInfo } from '@/actions/settings'
+import BotTrainingForm from '@/components/forms/settings/bot-training'
 import SettingsForm from '@/components/forms/settings/form'
 import InfoBar from '@/components/infobar'
+import ProductTable from '@/components/products'
 import { redirect } from 'next/navigation'
 import React from 'react'
 
@@ -14,18 +16,19 @@ const DomainSettingsPage = async ({ params }: Props) => {
     <>
       <InfoBar />
       <div className="overflow-y-auto w-full chat-window flex-1 h-0">
-        <h1>domain settings page</h1>
-        {/* <SettingsForm
+        <h1>domain settings </h1>
+        <h2>settings form</h2>
+        <SettingsForm
           plan={domain.subscription?.plan!}
           chatBot={domain.domains[0].chatBot}
           id={domain.domains[0].id}
           name={domain.domains[0].name}
-        /> */}
-        {/* <BotTrainingForm id={domain.domains[0].id} />
+        />
+        <BotTrainingForm id={domain.domains[0].id} />
         <ProductTable
           id={domain.domains[0].id}
           products={domain.domains[0].products || []}
-        /> */}
+        />
       </div>
     </>
   )
